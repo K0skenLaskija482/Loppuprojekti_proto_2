80a5e93d37e9be79e08e12ff4b0093e79a17a0cd
Savelius Milla <s2401247@edu.bc.fi>
2026-09-11 12:58:38 +0300

    Liikkumis koodi kesken
    Päätin vaihtaa miten teen liikkumis koodin

--- Assets/Scripts/WaypointManager.cs
+++ Assets/Scripts/WaypointManager.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class WaypointManager : MonoBehaviour
 {
+    public List<Transform> waypoints = new List<Transform>();
+    public bool isMoving;
+    public int waypointIndex;
+    [SerializeField] float movespeed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartMoving();
+    }
+
+    public void StartMoving()
+    {
+        waypointIndex = 0;
+        isMoving = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isMoving) { return; }
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, Time.deltaTime*movespeed);
     }
 }
